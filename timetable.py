1b07c896f04872d97475e278dee2f6dc597ec249
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2025-04-11 09:40:22 +0900

    タプルをディクトに変換

--- timetable.py
+++ timetable.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, Blueprint, request, session, flash,jsonify
-import db
+import db,json
 
 
 timetable_bp = Blueprint('timetable', __name__)
@@ -9,4 +9,14 @@ def course_register():
     
     subject_list = db.get_subjects()
     
-    return render_template('course_register.html' , subject_list=subject_list)
+    keys = ['id', 'name', 'teacher', 'schedule']
+    dict_list = [dict(zip(keys, row)) for row in subject_list]
+    
+    print(dict_list)
+    
+    # json_list = json.dumps(dict_list, ensure_ascii=False)
+    
+    # print(json_list)
+
+    
+    return render_template('syllabusregistration.html' ,dict_list=dict_list)
