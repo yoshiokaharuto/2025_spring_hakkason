5db0caa62c99953d6aa5f3007ed6cf4fa870c5d7
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2025-04-11 12:26:58 +0900

    履修登録完成

--- timetable.py
+++ timetable.py
@@ -4,7 +4,7 @@ import db,json
 
 timetable_bp = Blueprint('timetable', __name__)
 
-@timetable_bp.route('/course_register')
+@timetable_bp.route('/course_register', methods=['GET'])
 def course_register():
     
     subject_list = db.get_subjects()
@@ -12,11 +12,16 @@ def course_register():
     keys = ['id', 'name', 'teacher', 'schedule']
     dict_list = [dict(zip(keys, row)) for row in subject_list]
     
-    print(dict_list)
-    
-    # json_list = json.dumps(dict_list, ensure_ascii=False)
-    
-    # print(json_list)
-
-    
     return render_template('syllabusregistration.html' ,dict_list=dict_list)
+
+@timetable_bp.route('/register_subject', methods=['POST'])
+def register_subject():
+    
+    user_id = session.get('user_id')
+    subject_data = request.form.getlist('subjects')
+    
+    for data in subject_data:
+        success = db.register_subject(user_id,data)
+        if not success:
+            return redirect(url_for('timetable.course_register'))
+    return redirect(url_for('main'))
