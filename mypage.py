cf0b85220bf98d8bea8e04230cdf13cd2d66bd1c
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2025-04-12 11:10:44 +0900

    アカウントデータ取得

--- mypage.py
+++ mypage.py
@@ -7,4 +7,9 @@ mypage_bp = Blueprint('mypage', __name__)
 @mypage_bp.route('/mypage', methods=['GET'])
 def mypage():
     
+    account_id = session.get('user_id')
+    account_data = db.get_account_data(account_id)
+    
+    
+    
     return render_template('mypage.html')
